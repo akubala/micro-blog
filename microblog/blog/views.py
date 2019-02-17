b3b2e3243d571e9aa2753ff08a0d46f7c1e6d8af
Adrian Kubala <adr.kubala@gmail.com>
2019-02-17 20:15:47 +0100

    Add basic templates logic

--- microblog/blog/views.py
+++ microblog/blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'akubala',
+        'title': 'test post 1',
+        'content': 'test content 1',
+        'date': 'test-date-example'
+    },
+    {
+        'author': 'akubala',
+        'title': 'test post 2',
+        'content': 'test content 2 ',
+        'date': 'test-date-example'
+    }
+]
+
 
 def home_view(request):
-    return HttpResponse('<h1>MicroBlog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about_view(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'blog/about.html')
