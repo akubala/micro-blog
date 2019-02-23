2e9d25f4e630f0965769cb97a0658c2ac42a004d
Adrian Kubala <adr.kubala@gmail.com>
2019-02-23 18:45:14 +0100

    Add home and post details class-based views

--- microblog/blog/views.py
+++ microblog/blog/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
-def home_view(request):
+def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
-def about_view(request):
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+def about(request):
     return render(request, 'blog/about.html')
