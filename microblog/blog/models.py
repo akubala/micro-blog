743df3926ddbad2df0a6abd46a03b5d9bdac020c
Adrian Kubala <adr.kubala@gmail.com>
2019-02-24 16:43:33 +0100

    Add database dummy data

--- microblog/blog/models.py
+++ microblog/blog/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(max_length=512)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
